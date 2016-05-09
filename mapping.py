8005af89c231480158e786ecb150df21b2d88751
Emily <emilygarcia@student.hartnell.edu>
2016-05-09 15:29:38 -0700

    FINAL (python 2.7 ver)

--- mapping.py
+++ mapping.py
@@ -8,15 +8,15 @@ import numpy as np
 	
 # make sure the value of resolution is a lowercase L,
 #  for 'low', not a numeral 1
-map = Basemap(projection='mill', lat_0=36, lon_0=-119,
+mapp = Basemap(projection='mill', lat_0=36, lon_0=-119,
     resolution = 'h', area_thresh = .1,
     llcrnrlon=-130, llcrnrlat=31,
     urcrnrlon=-109, urcrnrlat=43)
  
-map.drawcoastlines()
-map.drawcountries()
-map.fillcontinents(color='coral')
-map.drawmapboundary()
-map.drawstates()
+mapp.drawcoastlines()
+mapp.drawcountries()
+mapp.fillcontinents(color='coral')
+mapp.drawmapboundary()
+mapp.drawstates()
  
 plt.show()
