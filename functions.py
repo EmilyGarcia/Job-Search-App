8c2422493b8c99b5d32f34361fd372b18935a5ad
Emily <emilygarcia@student.hartnell.edu>
2016-05-10 11:41:44 -0700

    Made Changes to GUI

--- functions.py
+++ functions.py
@@ -9,39 +9,43 @@ import numpy as np
 ##################################################################################################################################
 ##############################################           FUNCTIONS          ######################################################
 ##################################################################################################################################
-
+def parseDesc ( desc ):
+	for word in desc:
+		finalDesc = finalDesc + word
+		"""if (word == "<"):
+			finalDesc = finalDesc + (word + 3)
+		else:
+			finalDesc = finalDesc + word"""
+	return finalDesc
 def openDescription1( job1 ):
 	descWindow1 = Tkinter.Tk()
+	descWindow1.title(job1['title'])
 	jobDesc1 = job1['description']
-	print (job1['location'])
-	msg1 = Message(descWindow1, text= jobDesc1)
-	msg1.config(bg='lightgray', font=('times', 12))
+	msg1 = Message(descWindow1, text= jobDesc1, font=('times', 12), fg='MediumPurple4', bg='light cyan')
 	msg1.pack()
 def openDescription2( job2 ):
 	descWindow2 = Tkinter.Tk()
+	descWindow2.title(job2['title'])
 	jobDesc2 = job2['description']
-	print (job2['location'])
-	msg2 = Message(descWindow2, text= jobDesc2)
-	msg2.config(bg='lightgray', font=('times', 12))
+	msg2 = Message(descWindow2, text= jobDesc2, font=('times', 12), fg='MediumPurple4', bg='light cyan', relief = FLAT)
 	msg2.pack()
 def openDescription3( job3 ):
 	descWindow3 = Tkinter.Tk()
+	descWindow3.title(job3['title'])
 	jobDesc3 = job3['description']
-	print (job3['location'])
-	msg3 = Message(descWindow3, text= jobDesc3)
-	msg3.config(bg='lightgray', font=('times', 12))
+	msg3 = Message(descWindow3, text= jobDesc3, font=('times', 12), fg='MediumPurple4', bg='light cyan', relief = FLAT)
 	msg3.pack()
 def openDescription4( job4 ):
 	descWindow4 = Tkinter.Tk()
+	descWindow4.title(job2['title'])
 	jobDesc4 = job4['description']
-	msg4 = Message(descWindow4, text= jobDesc4)
-	msg4.config(bg='lightgray', font=('times', 12))
+	msg4 = Message(descWindow4, text= jobDesc4, font=('times', 12), fg='MediumPurple4', bg='light cyan', relief = FLAT)
 	msg4.pack()
 def openDescription5( job5 ):
 	descWindow5 = Tkinter.Tk()
+	descWindow5.title(job2['title'])
 	jobDesc5 = job5['description']
-	msg5 = Message(descWindow5, text= jobDesc5)
-	msg5.config(bg='lightgray', font=('times', 12))
+	msg5 = Message(descWindow5, text= jobDesc5, font=('times', 12), fg='MediumPurple4', bg='light cyan', relief = FLAT)
 	msg5.pack()
 def makeRequest( entry ):
 	# Gets city location entered
@@ -66,31 +70,31 @@ def displayJobs( entry, master ):
 	# Button 1
 	desc1 = partial( openDescription1,job1 )
 	
-	b1 = Button( master, command= desc1, text= job1['title'], width= 40 )
+	b1 = Button( master, command= desc1, text= job1['title'], width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 	b1.grid( row=4, column=0, sticky=W, pady=4 )
 	
 	# Button 2
 	desc2 = partial( openDescription2,job2 )
 	
-	b2 = Button( master, command= desc2, text= job2['title'], width= 40 )
+	b2 = Button( master, command= desc2, text= job2['title'], width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT )
 	b2.grid( row=5, column=0, sticky=W, pady=4 )
 	
 	# Button 3
 	desc3 = partial( openDescription3,job3 )
 	
-	b3 = Button( master, command= desc3, text= job3['title'], width= 40 )
+	b3 = Button( master, command= desc3, text= job3['title'], width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT )
 	b3.grid( row=6, column=0, sticky=W, pady=4 )
 	
 	# Button 4
 	desc4 = partial( openDescription4,job4 )
 	
-	b4 = Button( master, command= desc4, text= job4['title'], width= 40 )
+	b4 = Button( master, command= desc4, text= job4['title'], width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT )
 	b4.grid( row=7, column=0, sticky=W, pady=4 )
 	
 	# Button 5
 	desc5 = partial( openDescription5,job5 )
 	
-	b5 = Button( master, command= desc5, text= job5['title'], width= 40 )
+	b5 = Button( master, command= desc5, text= job5['title'], width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT )
 	b5.grid( row=8, column=0, sticky=W, pady=4 )
 """def getLat():
 	
@@ -119,5 +123,5 @@ def mapThat():
 	
 	labels = [job1['title'], job2['title'], job3['title']]
 	for label, xpt, ypt in zip(labels, x, y):
-    		plt.text(xpt+10000, ypt+5000, label)
+    		plt.text(xpt+30000, ypt+6000, label)
 	plt.show()
--- jobs.py
+++ jobs.py
@@ -9,32 +9,47 @@ from functions import *
 
 # main gui window
 master = Tkinter.Tk()
+master.title("Job Search")
+master.configure( bg='light cyan', padx = 40, pady = 30)
 
-# enter city prompt
-Label(master, text="City").grid(row=0)
+# title - grid 0
+#Label(master, text = "JOB SEARCH AND VISUALIZATION", height = 3, fg='MediumPurple4', justify=CENTER, bg='light cyan' ).grid(row=0, column=0)
 
-# entry box
+# enter city prompt - grid 1
+Label(master, width = 34, height = 2, fg='MediumPurple4', bg='turquoise' ).grid(row=1, column=0, sticky=W)
+Label(master, text="Enter City", width = 10, fg='MediumPurple4', bg='turquoise' ).grid(row=1, column=0, sticky=W)
+
+# entry box - grid 1
 e1 = Entry(master)
-e1.grid(row=0, column=1) # assigns location in master
+e1.configure( fg='MediumPurple4', width= 20, relief = FLAT )
+e1.grid(row=1, column=0) # assigns location in master
 
-# finds five job listings
+# finds five job listings - grid 1
 jobs = partial(displayJobs, e1, master)
-Button(master, text="Find", command=jobs).grid(row=0, column=2, sticky=W, pady=4)
+Button(master, text="Find", command=jobs, width= 5, height = 1, fg='MediumPurple4', bg='turquoise', highlightbackground='turquoise', relief = FLAT).grid(row=1, column=0, sticky=E, pady=10)
 
-# initial buttons w/ default text
-b1 = Button(master, text= "Job 1", width= 40)
+# spacing - grid 2
+Label(master, bg='light cyan').grid(row=2)
+
+# initial buttons w/ default text - grids 4-8
+b1 = Button(master, text= "Job 1", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b1.grid(row=4, column=0, sticky=W, pady=4)
-b2 = Button(master, text= "Job 2", width= 40)
+b2 = Button(master, text= "Job 2", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b2.grid(row=5, column=0, sticky=W, pady=4)
-b3 = Button(master, text= "Job 3", width= 40)
+b3 = Button(master, text= "Job 3", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b3.grid(row=6, column=0, sticky=W, pady=4)
-b4 = Button(master, text= "Job 4", width= 40)
+b4 = Button(master, text= "Job 4", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b4.grid(row=7, column=0, sticky=W, pady=4)
-b5 = Button(master, text= "Job 5", width= 40)
+b5 = Button(master, text= "Job 5", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b5.grid(row=8, column=0, sticky=W, pady=4)
 
-Button(master, text='Map', width= 10, command= mapThat).grid(row=9, column=0, sticky=W, pady=4)
-Button(master, text='Quit', command=master.quit, width= 10).grid(row=9, column=1, sticky=W, pady=4)
+# spacing - grid 9
+Label(master, bg='light cyan').grid(row=9)
+
+# maps five jobs - grid 10
+Button(master, text='Map', width= 10, command= mapThat, fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT).grid(row=10, column=0, sticky=W, pady=4)
+# quit - grid 10
+Button(master, text='Quit', command=master.quit, width= 10, fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT).grid(row=10, column=0, sticky=E, pady=4)
 
 mainloop()
 
