0364d4b8d87b8d9e7381fd2050182502acf610f3
Emily <emilygarcia@student.hartnell.edu>
2016-05-13 13:07:58 -0700

    Final

--- jobs.py
+++ jobs.py
@@ -7,31 +7,34 @@ from functions import *
 ##############################################              GUI              ######################################################
 ###################################################################################################################################
 
-# main gui window
+# Main GUI window
 master = Tkinter.Tk()
 master.title("Job Search")
 master.configure( bg='light cyan', padx = 40, pady = 30)
 
-# title - grid 0
-#Label(master, text = "JOB SEARCH AND VISUALIZATION", height = 3, fg='MediumPurple4', justify=CENTER, bg='light cyan' ).grid(row=0, column=0)
+# Enter city prompt - row 1
+turquoiseBg = Label(master, width = 34, height = 2, fg='MediumPurple4', bg='turquoise' )
+turquoiseBg.grid(row=1, column=0, sticky=W)
+promptLabel = Label(master, text="Enter City", width = 10, fg='MediumPurple4', bg='turquoise' )
+promptLabel.grid(row=1, column=0, sticky=W)
 
-# enter city prompt - grid 1
-Label(master, width = 34, height = 2, fg='MediumPurple4', bg='turquoise' ).grid(row=1, column=0, sticky=W)
-Label(master, text="Enter City", width = 10, fg='MediumPurple4', bg='turquoise' ).grid(row=1, column=0, sticky=W)
-
-# entry box - grid 1
+# Entry box - row 1
 e1 = Entry(master)
 e1.configure( fg='MediumPurple4', width= 20, relief = FLAT )
 e1.grid(row=1, column=0) # assigns location in master
 
-# finds five job listings - grid 1
+# Finds 5 job listings - row 1
 jobs = partial(displayJobs, e1, master)
-Button(master, text="Find", command=jobs, width= 5, height = 1, fg='MediumPurple4', bg='turquoise', highlightbackground='turquoise', relief = FLAT).grid(row=1, column=0, sticky=E, pady=10)
 
-# spacing - grid 2
-Label(master, bg='light cyan').grid(row=2)
+findBtn = Button(master, text="Find", command=jobs, width= 5, height = 1)
+findBtn.configure(fg='MediumPurple4', bg='turquoise', highlightbackground='turquoise', relief = FLAT)
+findBtn.grid(row=1, column=0, sticky=E, pady=4)
 
-# initial buttons w/ default text - grids 4-8
+# Spacing - row 2
+spacing1 = Label(master, text="First 5 Jobs in Search Results", fg='MediumPurple4', bg='light cyan')
+spacing1.grid(row=2)
+
+# Initial buttons w/ default text - rows 4-9
 b1 = Button(master, text= "Job 1", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b1.grid(row=4, column=0, sticky=W, pady=4)
 b2 = Button(master, text= "Job 2", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
@@ -43,13 +46,19 @@ b4.grid(row=7, column=0, sticky=W, pady=4)
 b5 = Button(master, text= "Job 5", width= 40, fg='MediumPurple4', bg='DarkOliveGreen3', relief = FLAT)
 b5.grid(row=8, column=0, sticky=W, pady=4)
 
-# spacing - grid 9
-Label(master, bg='light cyan').grid(row=9)
+# Spacing - row 10
+spacing2 = Label(master, bg='light cyan')
+spacing2.grid(row=10)
 
-# maps five jobs - grid 10
-Button(master, text='Map', width= 10, command= mapThat, fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT).grid(row=10, column=0, sticky=W, pady=4)
-# quit - grid 10
-Button(master, text='Quit', command=master.quit, width= 10, fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT).grid(row=10, column=0, sticky=E, pady=4)
+# Map Button - row 11
+mapBtn = Button(master, text='Map', command= mapThat, width= 10)
+mapBtn.configure(fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT)
+mapBtn.grid(row=11, column=0, sticky=W, pady=4)
+
+# Quit Button - row 11
+quitBtn = Button(master, text='Quit', command=master.quit, width= 10)
+quitBtn.configure(fg='MediumPurple4', bg='light salmon', highlightbackground='light salmon', relief = FLAT)
+quitBtn.grid(row=11, column=0, sticky=E, pady=4)
 
 mainloop()
 
